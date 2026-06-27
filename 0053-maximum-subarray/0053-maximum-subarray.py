2422a7f50d7b81adf469eca97e4605a0a4bc83d7
Bhanu-prakash-pokuri <pokuribhanu89@gmail.com>
2026-06-27 11:09:07 +0530

    Time: 41 ms (38.13%), Space: 31.5 MB (43.91%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -5,6 +5,6 @@ class Solution:
         for i in range(len(nums)):
 
             cur+=nums[i]
-            max_sum=max(max_sum,cur)
+            max_sum=cur if cur>max_sum else max_sum
             cur=0 if cur<0 else cur
         return max_sum 
