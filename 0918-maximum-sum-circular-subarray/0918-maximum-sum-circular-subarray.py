5871d92da63a9330a48df93856750380776406fe
Bhanu-prakash-pokuri <pokuribhanu89@gmail.com>
2026-06-27 13:19:32 +0530

    Time: 39 ms (83.87%), Space: 24.2 MB (32.17%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -8,9 +8,9 @@ class Solution:
         total=sum(nums)
         for i in range(n):
             currmax=nums[i] if nums[i]>currmax+nums[i] else currmax+nums[i]
-            max_sum=max(currmax,max_sum)
+            max_sum=currmax if currmax>max_sum else max_sum
             currmin=nums[i] if nums[i]<currmin+nums[i] else currmin+nums[i]
-            min_sum=min(currmin,min_sum)
+            min_sum=currmin if currmin<min_sum else min_sum
         if max_sum<0:
             return max_sum
         return max(max_sum,total-min_sum)
