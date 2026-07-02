0c2be2294c509b275bd62bbf180a02f12e54fc7b
Bhanu-prakash-pokuri <pokuribhanu89@gmail.com>
2026-07-02 10:13:55 +0530

    Time: 0 ms (100%), Space: 12.4 MB (18.3%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -4,21 +4,35 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        stack=[]
-        for i in s:
-            if i=="(":
-                stack.append(i)
-            else:
-                if not stack:
-                    stack.append(i)
-                    continue
+        # stack=[]
+        # for i in s:
+        #     if i=="(":
+        #         stack.append(i)
+        #     else:
+        #         if not stack:
+        #             stack.append(i)
+        #             continue
                 
 
-                t=stack[-1]
-                if i==")" and t=="(":
-                    stack.pop()
+        #         t=stack[-1]
+        #         if i==")" and t=="(":
+        #             stack.pop()
                 
-                else:
-                    stack.append(i)
-        return len(stack)
+        #         else:
+        #             stack.append(i)
+        # return len(stack)
         
+
+        open_count = 0   
+        additions = 0    
+
+        for ch in s:
+            if ch == "(":
+                open_count += 1
+            else:  # ch == ')'
+                if open_count > 0:
+                    open_count -= 1
+                else:
+                    additions += 1
+
+        return additions + open_count
