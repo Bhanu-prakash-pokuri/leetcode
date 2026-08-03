235252ec25f1f0864c0eadc0993ed83a7d5de33d
Bhanu-prakash-pokuri <pokuribhanu89@gmail.com>
2026-08-03 13:14:53 +0530

    Time: 272 ms (75%), Space: 96.3 MB (77.77%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,17 +1,37 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+
+        # using dfs
+        # g=collections.defaultdict(list)
+        # for u,v in edges:
+        #     g[u].append(v)
+        #     g[v].append(u)
+        # def dfs(node,visited):
+        #     if node==destination:
+        #         return True
+        #     visited.add(node)
+        #     for n in g[node]:
+        #         if n not in visited:
+        #             if dfs(n,visited):
+        #                 return True
+        #     return False
+        # visited=set()
+        # return dfs(source,visited)
+
+        #using bfs
         g=collections.defaultdict(list)
         for u,v in edges:
             g[u].append(v)
             g[v].append(u)
-        def dfs(node,visited):
+        queue=deque([source])
+        visited=set([source])
+        while queue:
+            node=queue.popleft()
             if node==destination:
                 return True
-            visited.add(node)
             for n in g[node]:
                 if n not in visited:
-                    if dfs(n,visited):
-                        return True
-            return False
-        visited=set()
-        return dfs(source,visited)
+                    visited.add(n)
+                    queue.append(n)
+        return False
+       
