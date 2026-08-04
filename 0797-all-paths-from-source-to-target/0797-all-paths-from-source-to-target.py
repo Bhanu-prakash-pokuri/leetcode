30f993882871a82f7d90101a715aadfd12332f31
Bhanu-prakash-pokuri <pokuribhanu89@gmail.com>
2026-08-04 10:57:19 +0530

    Time: 4 ms (65.35%), Space: 20.5 MB (81.26%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,9 +1,7 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        d=defaultdict(list)
         n=len(graph)
-        for i in range(n):
-            d[i].append(graph[i])
+        
         stack=[(0,[0])]
         res=[]
         while stack:
