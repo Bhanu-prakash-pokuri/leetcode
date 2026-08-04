d2769dde3b774d8d803e47c3ef460f12a42cc89e
Bhanu-prakash-pokuri <pokuribhanu89@gmail.com>
2026-08-04 09:51:25 +0530

    Time: 0 ms (100%), Space: 19.6 MB (51.37%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -18,13 +18,13 @@ class Solution:
         # return image
 
         # using bfs
-        old, m, n = image[sr][sc], len(image), len(image[0])
-        if old != color: 
+        start, m, n = image[sr][sc], len(image), len(image[0])
+        if start != color: 
             q = deque([(sr, sc)])
             while q:
                 i, j = q.popleft()
                 image[i][j] = color
                 for x, y in ((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)):
-                    if 0 <= x < m and 0 <= y < n and image[x][y] == old: 
+                    if 0 <= x < m and 0 <= y < n and image[x][y] == start: 
                         q.append((x, y))
         return image
