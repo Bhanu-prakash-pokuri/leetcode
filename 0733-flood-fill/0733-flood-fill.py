88305519115fcf2acc570f0ab4f01f5b61aec9c9
Bhanu-prakash-pokuri <pokuribhanu89@gmail.com>
2026-08-04 09:45:05 +0530

    Time: 0 ms (100%), Space: 19.6 MB (44.61%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,17 +1,28 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        old = image[sr][sc]
-        if old == color:
-            return image
-        m = len(image)
-        n = len(image[0])
-        def dfs(i, j):
-            if i < 0 or i >= m or j < 0 or j >= n or image[i][j] != old:
-                return
-            image[i][j] = color
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
-        dfs(sr, sc)
+        # old = image[sr][sc]
+        # if old == color:
+        #     return image
+        # m = len(image)
+        # n = len(image[0])
+        # def dfs(i, j):
+        #     if i < 0 or i >= m or j < 0 or j >= n or image[i][j] != old:
+        #         return
+        #     image[i][j] = color
+        #     dfs(i + 1, j)
+        #     dfs(i - 1, j)
+        #     dfs(i, j + 1)
+        #     dfs(i, j - 1)
+        # dfs(sr, sc)
+        # return image
+
+        old, m, n = image[sr][sc], len(image), len(image[0])
+        if old != color: 
+            q = collections.deque([(sr, sc)])
+            while q:
+                i, j = q.popleft()
+                image[i][j] = color
+                for x, y in ((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)):
+                    if 0 <= x < m and 0 <= y < n and image[x][y] == old: 
+                        q.append((x, y))
         return image
