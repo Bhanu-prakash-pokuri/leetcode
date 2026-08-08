4c233ff49d96560ffef0503e29d748aee0af0255
Bhanu-prakash-pokuri <pokuribhanu89@gmail.com>
2026-08-08 10:24:36 +0530

    Time: 44 ms (11.57%), Space: 20 MB (93.07%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -1,29 +1,29 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        # def atmost(nums,goal):
-        #     c=0
-        #     l=0
-        #     r=0
-        #     s=0
-        #     for l in range (len(nums)):
-        #         s+=nums[l]
+        def atmost(nums,goal):
+            c=0
+            l=0
+            r=0
+            s=0
+            for r in range (len(nums)):
+                s+=nums[r]
             
-        #         while s>goal and r<=l:
-        #             s-=nums[r]
-        #             r+=1
-        #         c+=l-r+1
+                while s>goal and l<=r:
+                    s-=nums[l]
+                    l+=1
+                c+=r-l+1
             
             
-        #     return c
-        # return atmost(nums,goal)-atmost(nums,goal-1)
+            return c
+        return atmost(nums,goal)-atmost(nums,goal-1)
 
-        result = 0
-        prefix_sum = 0
-        mp = {0: 1}
-        for num in nums:
-            prefix_sum += num
-            result += mp.get(prefix_sum - goal, 0)
-            mp[prefix_sum] = mp.get(prefix_sum, 0) + 1
-        return result
+        # result = 0
+        # prefix_sum = 0
+        # mp = {0: 1}
+        # for num in nums:
+        #     prefix_sum += num
+        #     result += mp.get(prefix_sum - goal, 0)
+        #     mp[prefix_sum] = mp.get(prefix_sum, 0) + 1
+        # return result
 
         
